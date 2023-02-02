f32021baf4f4d31bad8f1d050aa211c6b16edcd0
nbonheur111 <56658293+nbonheur111@users.noreply.github.com>
2023-02-02 13:19:22 -0600

    basic functionality

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ const express = require('express')
 
 const app = express();
 
-app.listen(4000, () => {
-    res.send("Server listening on 4000...")
+app.get('/', (req, res) => {
+    res.send("Server is running by the way")
+})
+
+app.listen(5000, () => {
+    console.log("Server listening on 5000...")
 })
